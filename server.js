b5083f87142cf572d5d79e70ed83ec10be915741
olssoncharlie <charlie-112@hotmail.com>
2017-10-06 12:27:48 +0200

    hämta ut relevant data från mat api bereoende på ID

--- server.js
+++ server.js
@@ -2,25 +2,28 @@ var http = require('http');
 
 function getData(callback, res){
 
-
-  return http.get({
-      host: 'matapi.se',
-      path: '/foodstuff'
+    var id = '1544';
+    return http.get({
+        host: 'matapi.se',
+        path: '/foodstuff/' + id
     }, function(response) {
         var body = '';
         response.on('data', function(d){
-          body += d;
-    });
+            body += d;
+        });
         response.on('end', function() {
-          var dataResponse = JSON.parse(body)
-          //res.write(dataResponse);
+            var dataResponse = JSON.parse(body);
+            //res.write(dataResponse)
+            // console.log(dataResponse[5].name);
 
-          for(i = 0; i < dataResponse.length; i++){
-            console.log(dataResponse[i])
-          }
+            //for(i = 0; i < dataResponse.length; i++){
+            console.log(dataResponse.name);
+            //}
 
+            //}
+
+        });
     });
-  });
 
 };
 http.createServer(getData).listen(3000);
