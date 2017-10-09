c155362a549cdcc909c6008cd9a59e81d65a0d54
olssoncharlie <charlie-112@hotmail.com>
2017-10-09 19:46:00 +0200

    Filmapi söker nu baserat på IMDbID

--- filmapi.js
+++ filmapi.js
@@ -1,14 +1,19 @@
 console.log("Starting filmapi.js");
 const http = require('http');
+const fs = require('fs');
 
 module.exports.getMovie = (callback, res) => {
     // Fast and Furious 10 verkar vara senast tillagda. ID 5433140
     // Slumpar ett nummer mellan 1000000 och 5433140
     // bör vara sortering efter error då alla värden inte har film
     // bör även vara sortering efter TYPE då episoder och filmer delar id
-    var movieId = 1000000 + Math.floor(Math.random() * 5433140);
-    console.log(movieId)
+    //var movieId =  Math.floor(Math.random() * 5433140);
+
+    //Fil med 9000imdb ID läses in. Görs till lista. Slumpar lista
+    var movieList = fs.readFileSync('imdb_id.txt', 'UTF-8').toString().split("\r");
+    var movieId = movieList[Math.floor(Math.random()*movieList.length)];
     //var title = 'Titanic';
+
     return http.get({
         host: 'omdbapi.com',
         path: '/?i=tt' + movieId + '&apikey=6397a4d9'
@@ -19,10 +24,9 @@ module.exports.getMovie = (callback, res) => {
         });
         response.on('end', function() {
             var dataResponse = JSON.parse(body);
-            console.log(dataResponse);
-            //for(i = 0; i < dataResponse.length; i++){
-
-
+            console.log('Film: ' + dataResponse.Title + '\n' +
+                'Årtal ' + dataResponse.Released + '\n' +
+                'Genres : ' + dataResponse.Genre);
         });
     });
 
