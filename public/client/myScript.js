b3f6903981dcb0a09283a1628b42189999416906
Emma Indahl <emma.indahl@gmail.com>
2017-10-17 17:35:14 +0200

    frontendfixxxxxxx

--- public/client/myScript.js
+++ public/client/myScript.js
@@ -2,30 +2,39 @@ var snacksBtn = document.getElementById('snacksBtn');
 var movieBtn = document.getElementById('movieBtn');
 
 snacksBtn.addEventListener('click', function (event) {
-    axios.get('/showSnacks')
-        .then(data => {
-        var obj = data;
-    var result = JSON.stringify(obj.data);
-    window.alert(result);
+    var displaySnacksElement = document.getElementById('getResult2');
+    displaySnacksElement.innerHTML = '';
 
+    axios.get('/showSnacks')
+        .then(snacks => {
+        var snacksobj = snacks;
+        displaySnacksElement.innerHTML = generateSnacksHTMLOutput(snacksobj)
 })
 });
+
+function generateSnacksHTMLOutput(response) {
+    return  '<h5> REKOMENDERAT SNACKS TILL FILMEN </h5>' +
+        '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>';
+;}
+
 
 movieBtn.addEventListener('click', function (event) {
     // SRC generate html :::: https://medium.com/codingthesmartway-com-blog/getting-started-with-axios-166cb0035237
     var displayMovieElement = document.getElementById('getResult1');
+    $('.modal').modal();
     displayMovieElement.innerHTML = '';
+
 
     axios.get('/showMovie')
         .then(movie => {
             var obj = movie;
             displayMovieElement.innerHTML = generateMovieHTMLOutput(obj);
-    }
+    })
 });
 
 function generateMovieHTMLOutput(response) {
-    return  '<h4> DIN FILM! </h4>' +
-        '<pre>' + JSON.stringify(response.data.Title, null, '\t') + '</pre>' +
-        '<pre>' + JSON.stringify(response.data.Released, null, '\t') + '</pre>' +
-        '<pre>' + JSON.stringify(response.data.Genre, null, '\t') + '</pre>';
+    return '<h4> FILMTIPS </h4>' +
+        '<pre>' + '<h6>Film:</h6>' + JSON.stringify(response.data.Title, null, '\t') + '</pre>' +
+        '<pre>' + '<h6>År:</h6>' + JSON.stringify(response.data.Released, null, '\t') + '</pre>' +
+        '<pre>' + '<h6>Genre:</h6>' + JSON.stringify(response.data.Genre, null, '\t') + '</pre>';
 }
